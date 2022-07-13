d3db37cca1a3ea1ceee419c7ff8b6988b4159cd2
MikeNichols1371 <mnich13711@gmail.com>
2022-07-13 15:45:02 -0400

    Finished Exercise 1

---
+++ Classes Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using Classes_Exercise;
+
+var car1 = new Car()
+{
+    Make = "Toyota",
+    Model = "Camry",
+    Year = 2022
+};
+
+Console.WriteLine(car1.Make);
+Console.WriteLine(car1.Model);
+Console.WriteLine(car1.Year);
